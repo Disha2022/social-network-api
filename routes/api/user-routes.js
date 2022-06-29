3684cda6b2bed85487dcf35f3716d84cc93a7404
disha <mumudb@gmail.com>
2022-06-28 22:54:06 -0400

    add user routes and controller

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
+
+// /api/users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+
+module.exports = router;
